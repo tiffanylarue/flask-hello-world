0146af7f3373c726f9ea5189d519aa80e373fa72
tiffanylarue <tiffanyllarue@gmail.com>
2025-03-31 17:25:19 +0000

    updates to app and reqts

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Tiffany LaRue in 3308!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://tiffsdb_user:m6bgJHymMgWBsEaFCzNWnwwCtd935qMS@dpg-cvlcrrq4d50c73e5akl0-a/tiffsdb")
+    conn.close()
+    return "Database connection successful!"
